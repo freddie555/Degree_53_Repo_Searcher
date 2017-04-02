681a158bd3f9ba746fcf0df3536ea05a839b06e6
Freddie Freeston <freddiefreeston@Freddies-MacBook-Pro.local>
2017-04-02 02:56:02 +0100

    Add stylesheet

--- spec/SearchBar.spec.js
+++ spec/SearchBar.spec.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import renderer from 'react-test-renderer';
 import SearchBar from '../src/components/SearchBar';
 
-test('CheckboxWithLabel changes the text after click', () => {
-  // Render a checkbox with label in the document
-  const checkbox = shallow(
-    <SearchBar labelOn="On" labelOff="Off" />
+describe('CheckboxWithLabel changes the text after click', () => {
+  it('should render a label', () => {
+    console.log('in test');
+    const component = renderer.create(
+    <SearchBar />
   );
-
-  expect(checkbox.text()).toEqual('Off');
-
-  checkbox.find('input').simulate('change');
-
-  expect(checkbox.text()).toEqual('On');
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+  });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+var webpack = require('webpack');
 const bundleFolder = path.join(__dirname, 'public', 'js');
 
-module.exports = {
+const config = {
   entry: {
     app: ['./src/index.js']
   },
@@ -34,3 +35,28 @@ module.exports = {
     stats: 'errors-only'
   }
 };
+
+// if (process.env.HOT) {
+//   config.devtool = 'eval'; // Speed up incremental builds
+//   config.entry['index.ios'].unshift('react-native-webpack-server/hot/entry');
+//   config.entry['index.ios'].unshift('webpack/hot/only-dev-server');
+//   config.entry['index.ios'].unshift('webpack-dev-server/client?http://localhost:8082');
+//   config.output.publicPath = 'http://localhost:8082/';
+//   config.plugins.unshift(new webpack.HotModuleReplacementPlugin());
+//   config.module.loaders[0].query.plugins.push('react-transform');
+//   config.module.loaders[0].query.extra = {
+//     'react-transform': [{
+//       target: 'react-transform-hmr',
+//       imports: ['react-native'],
+//       locals: ['module']
+//     }]
+//   };
+// }
+//
+// // Production config
+// if (process.env.NODE_ENV === 'production') {
+//   config.plugins.push(new webpack.optimize.OccurrenceOrderPlugin());
+//   config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+// }
+
+module.exports = config;
